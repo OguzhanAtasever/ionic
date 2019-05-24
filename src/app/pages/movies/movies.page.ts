8371a1d68b7bc9d17f2590ab3c927cf4adc4a3cf
OguzhanAtasever <oguzhanatasever@gmail.com>
2019-05-24 16:46:55 +0300

    serch is done

--- src/app/pages/movies/movies.page.ts
+++ src/app/pages/movies/movies.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {MovieService, SearchType} from '../../services/movie.service';
 
 @Component({
   selector: 'app-movies',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesPage implements OnInit {
 
-  constructor() { }
+  results: Observable<any>;
+  searchTerm = '';
+  type: SearchType = SearchType.all;
+  constructor(private movieService: MovieService) { }
 
   ngOnInit() {
+  }
+  searchChanged(){
+    this.results = this.movieService.searchData(this.searchTerm, this.type);
   }
 
 }
